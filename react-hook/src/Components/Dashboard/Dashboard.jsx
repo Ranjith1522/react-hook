da9a6e6db66e2a3d025e0cc78f7d355a33d49df3
ranjith <ranjithkankati2@gmail.com>
2025-11-27 23:25:30 +0530

    react hooks implementation

---
+++ react-hook/src/Components/Dashboard/Dashboard.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+function Dashboard() {
+
+
+    return(
+
+        <>
+        <h3 style={{color:'black'}}>Dashboard Component</h3>
+        </>
+    )
+}
+
+export default Dashboard
