ec7046d0d684c6bb27af38f5bc31598859d59cfc
ranjith <ranjithkankati2@gmail.com>
2025-11-27 23:37:30 +0530

    added axios

--- react-hook/src/Components/Dashboard/Dashboard.jsx
+++ react-hook/src/Components/Dashboard/Dashboard.jsx
@@ -1,12 +1,22 @@
 import React from "react"
+import axios from 'axios';
+
 
 function Dashboard() {
 
+    function getProducts() {
+        axios.get("https://api.escuelajs.co/api/v1/products");
+
+       console.log(products);
+    }
+    
 
     return(
 
         <>
         <h3 style={{color:'black'}}>Dashboard Component</h3>
+        <button onClick={()=>{getProducts}}>click</button>
+
         </>
     )
 }
