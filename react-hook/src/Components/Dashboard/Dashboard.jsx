c6f535650df79515bb97ed20290e8e7ff16dbcac
ranjith <ranjithkankati2@gmail.com>
2025-11-28 23:07:35 +0530

    added dashboard component with useEffect

--- react-hook/src/Components/Dashboard/Dashboard.jsx
+++ react-hook/src/Components/Dashboard/Dashboard.jsx
@@ -1,22 +1,39 @@
-import React from "react"
+import React, { useState } from "react"
 import axios from 'axios';
+import { useEffect } from "react";
+import './Dashboard.css'
 
 
 function Dashboard() {
 
-    function getProducts() {
-        axios.get("https://api.escuelajs.co/api/v1/products");
+    const [products, setProducts] = useState([]);
 
-       console.log(products);
+    async function getProducts() {
+        const res = await axios.get("https://api.escuelajs.co/api/v1/products");
+        setProducts(res.data);
     }
-    
 
-    return(
+    useEffect(() => {
+        getProducts();
+    }, [])
 
+
+    return (
         <>
-        <h3 style={{color:'black'}}>Dashboard Component</h3>
-        <button onClick={()=>{getProducts}}>click</button>
+            <h3 style={{ color: 'black' }}>Dashboard Component</h3>
+            <div className="card-container">
+                {
+                    products.map((ele) => {
 
+                        return (
+                            <div className="card" key={ele.id}>
+                                {ele.title}
+                            </div>
+                        )
+
+                    })
+                }
+            </div>
         </>
     )
 }
